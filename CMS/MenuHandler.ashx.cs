7200376d7a33efd2fbd485c754bd165e9b6e2897
mustafizur8888 <mustafizur8888@gmail.com>
2017-07-12 01:26:44 +0600

    menu fixed

---
+++ CMS/MenuHandler.ashx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace CMS
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string MenuText { get; set; }
+        public int? ParentId { get; set; }
+        public List<Menu> List { get; set; }
+    }
+    public class MenuHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            Db db = new Db();
+            List<SqlParameter> sqlParameters = new List<SqlParameter>
+            {
+                new SqlParameter {Value = "1", ParameterName = "@RoleId"}
+            };
+            var listMenu = new List<Menu>();
+            DataSet ds = db.GetDataSet("RoleWiseMenu", sqlParameters);
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                Menu menu = new Menu
+                {
+                    Id = Convert.ToInt32(row["Id"]),
+                    MenuText = row["MenuText"].ToString(),
+                    ParentId = row["ParentId"] != DBNull.Value
+                        ? Convert.ToInt32(row["ParentId"])
+                        : (int?) null
+                };
+
+                listMenu.Add(menu);
+            }
+            
+            List<Menu> menuTree = GetMenuTree(listMenu, null);
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            context.Response.Write(js.Serialize(menuTree.OrderBy(x => x.Id).ToList()));
+        }
+        public List<Menu> GetMenuTree(List<Menu> list, int? parent)
+        {
+            return list.Where(x => x.ParentId == parent).Select(x => new Menu
+            {
+                Id = x.Id,
+                MenuText = x.MenuText,
+                ParentId = x.ParentId,
+                List = GetMenuTree(list, x.Id)
+            }).ToList();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
--- CMS/Site2.Master.cs
+++ CMS/Site2.Master.cs
@@ -1,21 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Text;
 using System.Web;
-using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace CMS
 {
-    public partial class UniSite : System.Web.UI.MasterPage
+    public partial class Site2 : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-        }
 
+        }
     }
 }
