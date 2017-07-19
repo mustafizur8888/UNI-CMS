6c694adbc2cb92575505e408dac3eeca59d1c72e
iftekharAli <ali.iftekhar343@gmail.com>
2017-07-20 02:00:19 +0600

    5 pages done

---
+++ CMS/Pages/Content/CreateCategory.aspx.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DAL;
+
+namespace CMS.Pages.Content
+{
+    public partial class CreateCategory : System.Web.UI.Page
+    {
+        private Db _db = new Db();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            divError.Visible = false;
+            divSucc.Visible = false;
+            if (!IsPostBack)
+            {
+                PortalMenuLoad();
+            }
+        }
+
+        private void PortalMenuLoad()
+        {
+            List<SqlParameter> sqlParameters = new List<SqlParameter>
+            {
+                new SqlParameter
+                {
+                    Value = "portalName",
+                    ParameterName = "@type"
+                },
+
+            };
+            DataSet dsMenuDataSet = _db.GetDataSet("sp_GetMenuNames", sqlParameters);
+            if (dsMenuDataSet != null && dsMenuDataSet.Tables[0].Rows.Count > 0)
+            {
+                ddlPortalMenu.DataSource = dsMenuDataSet;
+                ddlPortalMenu.DataTextField = "Name";
+                ddlPortalMenu.DataValueField = "id";
+                ddlPortalMenu.DataBind();
+            }
+        }
+
+        protected void btnCancel_OnClick(object sender, EventArgs e)
+        {
+            Clear();
+            //throw new NotImplementedException();
+        }
+
+        protected void btnEdit_OnClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            GridViewRow gvr = (GridViewRow)btn.NamingContainer;
+            string CategoryName = ((Label)gvr.FindControl("lblCategoryName")).Text;
+            string Status = ((Label)gvr.FindControl("lblIsActive")).Text;
+            string HasSubeMenu = ((Label)gvr.FindControl("lblHasSubManu")).Text;
+            string MasterId = ((HiddenField)gvr.FindControl("hiMasterId")).Value;
+            string PrimaryId = ((HiddenField)gvr.FindControl("hidId")).Value;
+            hidMasterMenuId.Value = MasterId;
+            hidIdPrimary.Value = PrimaryId;
+            checkIsActive.Checked = Status == "True";
+            checkHasSubMenu.Checked = HasSubeMenu == "True";
+            //txtMasterMenu.Text = MasterMenu;
+            txtCategory.Text = CategoryName;
+            ddlPortalMenu.SelectedValue = MasterId;
+            btnSave.Text = "Update";
+        }
+
+        protected void btnDelete_OnClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            GridViewRow gvr = (GridViewRow)btn.NamingContainer;
+            string idVal = ((HiddenField)gvr.FindControl("hidId")).Value;
+            int id = 0;
+            id = idVal != "" ? Convert.ToInt32(idVal) : 0;
+            List<SqlParameter> sqlParameters = new List<SqlParameter>
+            {
+                new SqlParameter{Value = "delete",ParameterName = "@type"},
+                new SqlParameter{Value = id,ParameterName = "@id"}
+            };
+
+            int count = 0;
+            try
+            {
+                count = _db.ExecuteNonQuery("sp_SetCategoryName", sqlParameters);
+            }
+            catch (Exception exception)
+            {
+                ShowErrorMsg(exception.Message);
+            }
+
+            if (count > 0)
+            {
+                ShowSuccMsg("Delete Successfully");
+                LoadCategory();
+            }
+            else
+            {
+                ShowErrorMsg("Failed To Delete");
+            }
+        }
+
+        private void Clear()
+        {
+            txtCategory.Text = String.Empty;
+            checkIsActive.Checked = false;
+            checkHasSubMenu.Checked = false;
+            hidMasterMenuId.Value = "";
+            hidIdPrimary.Value = "";
+            btnSave.Text = "Save";
+        }
+
+        protected void btnSave_OnClick(object sender, EventArgs e)
+        {
+            if (Validation())
+            {
+
+
+                string CategoryName = txtCategory.Text;
+                int MasterMenuId = Convert.ToInt32(ddlPortalMenu.SelectedValue);
+                string CreatedBy = String.Empty;
+                try
+                {
+                    CreatedBy = Session["User"].ToString();
+                }
+                catch
+                {
+                    CreatedBy = String.Empty;
+                }
+                // string CreatedBy = String.Empty;
+                string UpdatedBy = CreatedBy;
+                DateTime CreateDate = DateTime.Now;
+                bool isActive = checkIsActive.Checked;
+                bool HasSubMenu = checkHasSubMenu.Checked;
+                string type = hidMasterMenuId.Value;
+                string idVal = hidIdPrimary.Value;
+                int id = 0;
+                id = idVal != "" ? Convert.ToInt32(idVal) : 0;
+                bool status = checkIsActive.Checked;
+                type = type == "" ? "insert" : "update";
+                List<SqlParameter> sqlParameters = new List<SqlParameter>
+                {
+                    new SqlParameter{Value = type,ParameterName = "@type"},
+                    new SqlParameter{Value = CategoryName,ParameterName = "@CategoryName"},
+                    new SqlParameter{Value = ddlPortalMenu.SelectedValue,ParameterName = "@PortalId"},
+                    new SqlParameter{Value = CreatedBy,ParameterName = "@CreatedBy"},
+                    new SqlParameter{Value = UpdatedBy,ParameterName = "@UpdatedBy"},
+                    new SqlParameter{Value =CreateDate,ParameterName = "@CreateDate"},
+                    new SqlParameter{Value = CreateDate,ParameterName = "@UpdateDate"},
+                    new SqlParameter{Value = isActive,ParameterName = "@isActive"},
+                    new SqlParameter{Value = HasSubMenu,ParameterName = "@HasSubMenu"},
+                    new SqlParameter{Value = id,ParameterName = "@id"},
+
+                };
+                hidMasterMenuId.Value = "";
+                hidIdPrimary.Value = "";
+                int count = _db.ExecuteNonQuery("sp_SetCategoryName", sqlParameters);
+                if (count > 0)
+                {
+                    ShowSuccMsg(type == "update" ? "Updated Successfully" : "Saved Successfully");
+                    Clear();
+                    LoadCategory();
+
+                }
+            }
+        }
+
+        private void LoadCategory()
+        {
+            List<SqlParameter> sqlParameters = new List<SqlParameter>
+            {
+                new SqlParameter{Value = "select",ParameterName = "@type"},
+
+            };
+            DataSet ds = null;
+            ds = _db.GetDataSet("sp_SetCategoryName", sqlParameters);
+            grdMenu.DataSource = ds;
+            grdMenu.DataBind();
+        }
+
+        private void ShowSuccMsg(string msg)
+        {
+            lblSuccess.Text = msg;
+            divSucc.Visible = true;
+        }
+
+        private bool Validation()
+        {
+            bool result = true;
+            string msg = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(txtCategory.Text))
+            {
+                msg += "Category name is empty" + "<br>";
+            }
+            
+            if (ddlPortalMenu.Items.Count == 0)
+            {
+                msg += "Please select Portal from menu" + "<br>";
+            }
+         
+            //else
+            //{
+
+            //    string query = "Select count(*) from [tbl_MasterMenu] where MenuOrder='" + txtMenuOrder.Text + "'";
+            //    string value = _db.GetSingelValue(query);
+            //    if (!string.IsNullOrEmpty(value))
+            //    {
+            //        if (value != "0")
+            //        {
+            //            msg += "Menu order already exists" + "<br/>";
+            //        }
+            //    }
+
+            //}
+
+            if (!string.IsNullOrWhiteSpace(msg))
+            {
+                result = false;
+                ShowErrorMsg(msg);
+            }
+
+
+            return result;
+        }
+        private void ShowErrorMsg(string msg)
+        {
+            lblError.Text = msg;
+            divError.Visible = true;
+        }
+
+        protected void btnLoad_OnClick(object sender, EventArgs e)
+        {
+            LoadCategory();
+        }
+
+       
+    }
+}
---
+++ CMS/Pages/Content/CreateSubCategory.aspx
@@ -0,0 +1,135 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateSubCategory.aspx.cs" MasterPageFile="~/UniSite.Master" Inherits="CMS.Pages.Content.CreateSubCategory" %>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+
+
+        <div class="alert  alert-danger text-center" runat="server" id="divError">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <asp:Label runat="server" ID="lblError"></asp:Label>
+        </div>
+        <div class="alert  alert-success text-center" runat="server" id="divSucc">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <asp:Label runat="server" ID="lblSuccess"></asp:Label>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="form-group col-lg-offset-1 col-md-offset-1">
+            <label for="txtSubCategory" class="col-lg-2 col-md-2 control-label text-right">Sub-Category Name</label>
+            <div class="col-lg-4 col-md-6">
+                <asp:TextBox runat="server" CssClass="form-control" ID="txtSubCategory"></asp:TextBox>
+            </div>
+
+        </div>
+
+    </div>
+    <br />
+    <div class="row">
+        <div class="form-group col-lg-offset-1 col-md-offset-1">
+            <label for="ddlPortalMenu" class="col-lg-2 col-md-2 control-label text-right">Portal Name</label>
+            <div class="col-lg-4 col-md-6">
+                <%--<asp:TextBox runat="server" CssClass="form-control" ID="TextBox1"></asp:TextBox>--%>
+                <asp:DropDownList runat="server" CssClass="form-control" ID="ddlPortalMenu" AutoPostBack="True" OnSelectedIndexChanged="ddlMasterMenu_OnSelectedIndexChanged" />
+            </div>
+
+        </div>
+
+    </div>
+    <br />
+    <div class="row">
+        <div class="form-group col-lg-offset-1 col-md-offset-1">
+            <label for="ddlCategoryName" class="col-lg-2 col-md-2 control-label text-right">Category Name</label>
+            <div class="col-lg-4 col-md-6">
+                <asp:DropDownList runat="server" CssClass="form-control" ID="ddlCategoryName" AutoPostBack="True" />
+            </div>
+
+        </div>
+
+    </div>
+    <br />
+   
+    <div class="row">
+        <div class="form-group col-lg-offset-1 col-md-offset-1">
+            <label for="checkIsActive" class="col-lg-2 col-md-2 control-label text-right">Is Active? </label>
+            <div class="col-lg-4 col-md-6">
+                <asp:CheckBox runat="server" ID="checkIsActive"></asp:CheckBox>
+            </div>
+
+        </div>
+
+    </div>
+    <br />
+   
+
+    <div class="row">
+        <div class="form-group col-lg-offset-3 col-md-offset-3">
+
+            <%--<button type="reset" class="btn btn-danger">Cancel</button>--%>
+            <asp:Button runat="server" ID="btnCancel" CssClass="btn btn-danger" Text="Cancel" OnClick="btnCancel_OnClick" />
+            <asp:Button runat="server" ID="btnSave" CssClass="btn btn-primary" Text="Save" OnClick="btnSave_OnClick" />
+            <asp:Button runat="server" ID="btnLoad" CssClass="btn btn-primary" Text="LoadAll" OnClick="btnLoad_OnClick" />
+           
+
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col-md-12 col-lg-12 col-sm-12">
+            <asp:GridView ID="grdSubCategory" runat="server" CssClass="table table-striped table-hover  table-condensed " AutoGenerateColumns="False" GridLines="None">
+                <Columns>
+
+                    <asp:TemplateField HeaderText="SubMenuName">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="lblSubCategoryName" Text='<%# Bind("SubCategoryName") %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Is_Active">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="lblIsActive" Text='<%# Bind("Is_Active") %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Created_By">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="lblCreatedBy" Text='<%# Bind("CreatedBy") %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Updated_By">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="lblUpdatedBy" Text='<%# Bind("Update_By") %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="CreatedDate">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="lblCreatedDate" Text='<%# Bind("CreatedDate") %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Update_Date">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="lblUpdateDate" Text='<%# Bind("Update_Date") %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                   
+                  
+                    <asp:TemplateField HeaderText="Action">
+                        <ItemTemplate>
+                            <asp:HiddenField runat="server" ID="hiMasterId" Value='<%# Bind("CategoryId") %>' />
+                            <asp:HiddenField runat="server" ID="hidId" Value='<%# Bind("Id") %>' />
+                            <div class="btn-group btn-group-sm">
+                                <asp:Button runat="server" CssClass="btn btn-primary btn-sm " Text="Edit" ID="btnEdit" OnClick="btnEdit_OnClick" />
+                                <asp:Button runat="server" CssClass="btn btn-danger  btn-sm" Text="Delete" ID="btnDelete" OnClick="btnDelete_OnClick" />
+                            </div>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                </Columns>
+            </asp:GridView>
+        </div>
+        <asp:HiddenField runat="server" ID="hidIdPrimary" />
+        <asp:HiddenField runat="server" ID="hidMenuId" />
+    </div>
+
+
+</asp:Content>
+
